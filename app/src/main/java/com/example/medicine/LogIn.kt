06f7365035a5f9785581bf5de8f1c317260eb53e
walter bruno <walter_bruno21091987@hotmail.com>
2023-04-18 13:16:16 -0300

    actualizacion_con_navigation

--- app/src/main/java/com/example/medicine/LogIn.kt
+++ app/src/main/java/com/example/medicine/LogIn.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.example.medicine.databinding.FragmentLogInBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -20,7 +22,7 @@ class LogIn : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
-
+    lateinit var binding:FragmentLogInBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
@@ -34,7 +36,8 @@ class LogIn : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_log_in, container, false)
+        binding= DataBindingUtil.inflate(inflater,R.layout.fragment_log_in,container,false)
+        return binding.root
     }
 
     companion object {
--- app/src/main/java/com/example/medicine/MainActivity.kt
+++ app/src/main/java/com/example/medicine/MainActivity.kt
@@ -2,10 +2,12 @@ package com.example.medicine
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val navController=findNavController(R.id.nav_host_fragment_activity_main)
     }
 }
