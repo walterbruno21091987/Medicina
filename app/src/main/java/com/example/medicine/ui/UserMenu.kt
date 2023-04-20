fd24061245fd660c4c869a871e03c2062956c46a
walter bruno <walter_bruno21091987@hotmail.com>
2023-04-20 10:24:49 -0300

    1.1.1

--- app/src/main/java/com/example/medicine/ui/UserMenu.kt
+++ app/src/main/java/com/example/medicine/ui/UserMenu.kt
@@ -5,7 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import com.example.medicine.R
+import com.example.medicine.databinding.FragmentUserMenuBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -21,6 +23,7 @@ class UserMenu : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+    lateinit var binding:FragmentUserMenuBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,8 +37,13 @@ class UserMenu : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_user_menu, container, false)
+       binding= DataBindingUtil.inflate(inflater,R.layout.fragment_user_menu,container,false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
     }
 
     companion object {
