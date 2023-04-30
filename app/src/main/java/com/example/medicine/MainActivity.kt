2231af62fbd65b934aae7ae846fc498bcacc8c0a
walter bruno <walter_bruno21091987@hotmail.com>
2023-04-30 12:18:09 -0300

    agrego firebase,crashlitic,analitic

--- app/src/main/java/com/example/medicine/MainActivity.kt
+++ app/src/main/java/com/example/medicine/MainActivity.kt
@@ -8,6 +8,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         val navController=findNavController(R.id.nav_host_fragment_activity_main)
     }
 }
